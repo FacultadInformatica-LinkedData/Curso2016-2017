59352075287a794bf9a9fddfb390fa937f40edb6
Aitor Garcia Alvarez <aitorg.512@gmail.com>
2016-12-23 09:20:26 +0100

    Update app to open rdf packed in tar.gz

--- HandsOn/Group32/app/app.py
+++ HandsOn/Group32/app/app.py
@@ -1,14 +1,27 @@
+tar_filename = "..rdf/data.rdf.tar.gz" 
 filename = "..rdf/data.rdf" 
 
 import rdflib
 import rdfextras
-rdfextras.registerplugins() # so we can Graph.query()
+import tarfile
+import os
 
+ # Unpack the tar.gz
+tar = tarfile.open(tar_filename, "r:gz")
+tar.extractall()
+tar.close()
+
+rdfextras.registerplugins() # so we can Graph.query()
+ # library to load an RDF
 g=rdflib.Graph()
+ # Load the the local RDF
 g.parse(filename)
+ # Query
 results = g.query("""
 SELECT ?p ?o
 WHERE {
 <%s> ?p ?o.
-}
-""") 
+}""") 
+
+ # Remove the RDF
+os.remove(filename)
